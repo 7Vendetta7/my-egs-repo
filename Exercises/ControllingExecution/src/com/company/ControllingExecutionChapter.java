d53494fe9ed8ad8eb40c1433af94e389643921db
Argishti <argishtit@energizeglobal.com>
2021-07-15 14:22:52 +0400

    added new exercise with it's solution

--- Exercises/ControllingExecution/src/com/company/ControllingExecutionChapter.java
+++ Exercises/ControllingExecution/src/com/company/ControllingExecutionChapter.java
@@ -4,10 +4,11 @@ import java.util.Random;
 
 /**
  * Description: This class is designed for covering the most part of the exercises from the ThinkingInJava4e book.
- *              Each exercise represents a public static method and each description of exercise is written above
- *              the method.
+ * Each exercise represents a public static method and each description of exercise is written above
+ * the method.
  * Chapter:     Controlling Execution
- * @author      Argishti_tigranyan
+ *
+ * @author Argishti_tigranyan
  */
 
 public class ControllingExecutionChapter {
@@ -19,23 +20,21 @@ public class ControllingExecutionChapter {
      *              randomly generated value.
      */
 
-    public static void classificationOfInts(){
+    public static void classificationOfInts() {
 
         int firstInt, secondInt;
         Random random = new Random();
-        for (int i = 0; i < 25; i++){
+        for (int i = 0; i < 25; i++) {
 
             firstInt = random.nextInt(1000);
             secondInt = random.nextInt(1000);
 
-            if (firstInt > secondInt){
-                System.out.println(firstInt+" is greater than "+secondInt);
-            }
-            else if (firstInt == secondInt){
-                System.out.println(firstInt+" is equal to "+secondInt);
-            }
-            else{
-                System.out.println(firstInt+" is less than "+secondInt);
+            if (firstInt > secondInt) {
+                System.out.println(firstInt + " is greater than " + secondInt);
+            } else if (firstInt == secondInt) {
+                System.out.println(firstInt + " is equal to " + secondInt);
+            } else {
+                System.out.println(firstInt + " is less than " + secondInt);
             }
         }
     }
@@ -48,37 +47,35 @@ public class ControllingExecutionChapter {
      * Little Note: in our case we should push control+f2.
      */
 
-    public static void infiniteClassificationOfInts(){
+    public static void infiniteClassificationOfInts() {
 
         Random random = new Random();
 
         int firstInt, secondInt;
 
-        while (true){
+        while (true) {
 
             firstInt = random.nextInt(1000);
             secondInt = random.nextInt(1000);
 
-            if (firstInt > secondInt){
-                System.out.println(firstInt+" is greater than "+secondInt);
-            }
-            else if (firstInt == secondInt){
-                System.out.println(firstInt+" is equal to "+secondInt);
-            }
-            else{
-                System.out.println(firstInt+" is less than "+secondInt);
+            if (firstInt > secondInt) {
+                System.out.println(firstInt + " is greater than " + secondInt);
+            } else if (firstInt == secondInt) {
+                System.out.println(firstInt + " is equal to " + secondInt);
+            } else {
+                System.out.println(firstInt + " is less than " + secondInt);
             }
         }
     }
 
     /*
-     * Exercise:    3
+     * Exercise:    4
      * Description: Write a program that uses two nested for loops and the modulus
      *              operator (%) to detect and print prime numbers (integral numbers that are not evenly
      *              divisible by any other numbers except for themselves and 1).
      */
 
-    public static void primeNumbers(){
+    public static void primeNumbers() {
 
         StringBuilder primeNumbers = new StringBuilder("");
 
@@ -86,8 +83,8 @@ public class ControllingExecutionChapter {
 
             int counter = 0;
 
-            for(int num = i; num >= 1; num--) {
-                if(i%num == 0) {
+            for (int num = i; num >= 1; num--) {
+                if (i % num == 0) {
                     counter++;
                 }
             }
@@ -96,7 +93,39 @@ public class ControllingExecutionChapter {
             }
         }
 
-        System.out.println("Prime numbers from 1 to 100 are: "+primeNumbers);
+        System.out.println("Prime numbers from 1 to 100 are: " + primeNumbers);
+    }
+
+    /*
+     * Exercise:    9
+     * Description: A Fibonacci sequence is the sequence of numbers 1, 1, 2, 3, 5, 8, 13, 21,
+     *              34, and so on, where each number (from the third on) is the sum of the previous two. Create
+     *              a method that takes an integer as an argument and displays that many Fibonacci numbers
+     *              starting from the beginning, e.g., If you run java Fibonacci 5 (where Fibonacci is the
+     *              name of the class) the output will be: 1, 1, 2, 3, 5.
+     */
+
+    public static void fibonacciSequence(int quantity) throws IllegalArgumentException {
+
+        int previous = 1;
+        int current = 1;
+        int next = 2;
+
+        if (quantity <= 0) {
+            throw new IllegalArgumentException("The quantity must be greater or equal to 1");
+        } else if (quantity == 1) {
+            System.out.println(previous);
+        } else if (quantity == 2) {
+            System.out.println(previous + " " + current);
+        } else {
+            for (int i = 0; i < quantity; i++) {
+                System.out.print(previous + " ");
+                previous = current;
+                current = next;
+                next += previous;
+            }
+        }
+
     }
 
 }
